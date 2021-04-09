4e84f9e8783814cf04d1e2675c8dee59469d25be
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-09 14:08:36 +0300

    tu'

---
+++ Week-1/Day-5/PortfolioProject/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".fa-bars").click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        if($(window).scrollTop() <25){
+            $('.header').hide();
+        }
+        else{
+            $('.header').show();
+        }
+    })
+})
