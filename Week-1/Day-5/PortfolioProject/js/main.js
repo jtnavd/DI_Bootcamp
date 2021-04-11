cf2db4046ee91c312ee27609805b61b14cd378e7
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-11 13:22:40 +0300

    11/04/212-13.30

--- Week-1/Day-5/PortfolioProject/js/main.js
+++ Week-1/Day-5/PortfolioProject/js/main.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-    $(".fa-bars").click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
+// $(document).ready(function() {
+//     $(".fa-bars").click(function(){
+//         $(this).toggleClass('fa-times');
+//         $('.navbar').toggleClass('nav-toggle');
+//     });
 
-    $(window).on('scroll load',function(){
-        if($(window).scrollTop() <25){
-            $('.header').hide();
-        }
-        else{
-            $('.header').show();
-        }
-    })
-})
+//     $(window).on('scroll load',function(){
+//         if($(window).scrollTop() <25){
+//             $('.header').hide();
+//         }
+//         else{
+//             $('.header').show();
+//         }
+//     })
+// })
+
+//NAVBAR SCROLLING #################################################################
