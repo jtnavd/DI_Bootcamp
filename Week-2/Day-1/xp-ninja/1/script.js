7dd3271414a8cb57b15f8761e572b3dfbdcb65af
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-11 15:50:30 +0300

    test

---
+++ Week-2/Day-1/xp-ninja/1/script.js
@@ -0,0 +1,12 @@
+let sentence1="I can't find Nemo.";
+let sentence2="I found .";
+let sentence3="Dori is 's best firend";
+
+let searchWord = "Nemo"
+
+
+//RESULT
+console.log(searchWord);
+console.log(sentence1);
+console.log(sentence2);
+console.log(sentence3);
