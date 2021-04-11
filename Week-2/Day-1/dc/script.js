80a8f09e32fdeac0e4ec79ae4072d671ae566f96
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-11 20:44:45 +0300

    Daily Challenge

---
+++ Week-2/Day-1/dc/script.js
@@ -0,0 +1,16 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+let arr1=fruits.shift();
+console.log("remove banana = ",fruits);
+
+fruits.sort();
+console.log("alphabetical order = ",fruits);
+
+fruits.push("Kiwi");
+console.log("add Kiwi = ",fruits);
+
+let arr2=fruits.shift();
+console.log("remove apples = ", fruits);
+
+fruits.reverse();
+console.log("reverse order = ",fruits);
