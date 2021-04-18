d09ad5d45de1d5115877b598a25bf9fac093bf13
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-18 16:32:30 +0300

    mini project daily challenge

--- Week-2/Day-5/dc/1/script.js
+++ Week-2/Day-5/dc/1/script.js
@@ -1,7 +1,19 @@
-let num = 99;
+var num = 99;
+var subNum = 1;
 
-while(num >= 99){
- console.log
-    
-    num --;
-}
+do {
+    newNum = num - subNum;
+    console.log(num+" bottles of beer on the wall.");
+    console.log(num+" bottles of beer.");
+    console.log("Take "+ subNum+ " down, pass it around");
+    num-subNum;
+    subNum++;
+console.log(newNum + " bottles of beer Left on the wall.");}
+while (num > subNum);
+
+// function checkBottle(num) {
+//     if (num <=0){
+//         console.log("There's no bottle left on the wall.");
+
+//     }
+// }
