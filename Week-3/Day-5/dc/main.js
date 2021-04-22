7a335e340215f97937839cbfdef77d7fb4f15910
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-22 14:22:54 +0300

    mini project drum final

---
+++ Week-3/Day-5/dc/main.js
@@ -0,0 +1,5 @@
+let listTasks = [];
+
+function addTask(){
+    
+}
--- Week-3/Day-5/xp/1/main.js
+++ Week-3/Day-5/xp/1/main.js
@@ -1,12 +1,14 @@
-const container = document.querySelector('container')
-const btnblack = document.querySelector('button')
-const btnrgb = document.querySelector('button')
-const btnsize = document.querySelector('button')
-const buttonContainer = document.querySelector('button')
+const container = document.querySelector('.container')
+const btnrgb = document.querySelector('.button')
 
 function creatDivs(col , rows) {
     for(let i = 0;i < (col * rows); i++) {
         const div = document.createElement('div') 
+        div.style.border= '0.5px solid black';
+        div.style.width= '20px';
+        div.style.height= '20px';
+        container.style.maxheight= 100;
+        // container.style.maxWidth=100;
         container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
@@ -15,14 +17,15 @@ function creatDivs(col , rows) {
 
 creatDivs(16,16)
 
-function blackColor(){
-    const boxs = container.querySelectorAll('.box')
-    btnblack.textContent = 'Black';
-    btnblack.addEventListener('click', ()=>{
-        boxs.forEach(box=>box.addEventListener('mouseover', ()=>{
-            boxs.style.background = "black";
-        })) 
-    })
+// function blackColor(){
+//     const boxs = container.querySelectorAll('.box');
+//     btnblack = document.createAttribute("color")
+//     btnblack.textContent = 'Black';
+//     btnblack.addEventListener('click', ()=>{
+//         boxs.forEach(box=>box.addEventListener('mouseover', ()=>{
+//             boxs.style.background = "black";
+//         })) 
+//     })
 
-    buttonContainer.appendChild(btnblack).classList.add('btn')
-}
+//     buttonContainer.appendChild(btnblack).classList.add('btn')
+// }
