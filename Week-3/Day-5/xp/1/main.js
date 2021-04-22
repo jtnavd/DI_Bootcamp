a67dd3ff486cc7b6aecca4673ee0a16b3854584c
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-22 23:40:12 +0300

    mini project 1

--- Week-3/Day-5/xp/1/main.js
+++ Week-3/Day-5/xp/1/main.js
@@ -1,31 +1,78 @@
 const container = document.querySelector('.container')
-const btnrgb = document.querySelector('.button')
+const btnBlack = document.createElement('button')
+const btnRgb = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnSize = document.createElement('button')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
+
+window.onload = () => {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.background = 'black'
+    }))
+}
+
 
 function creatDivs(col , rows) {
     for(let i = 0;i < (col * rows); i++) {
         const div = document.createElement('div') 
-        div.style.border= '0.5px solid black';
-        div.style.width= '20px';
-        div.style.height= '20px';
-        container.style.maxheight= 100;
-        // container.style.maxWidth=100;
         container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
     }
 }
-
 creatDivs(16,16)
 
-// function blackColor(){
-//     const boxs = container.querySelectorAll('.box');
-//     btnblack = document.createAttribute("color")
-//     btnblack.textContent = 'Black';
-//     btnblack.addEventListener('click', ()=>{
-//         boxs.forEach(box=>box.addEventListener('mouseover', ()=>{
-//             boxs.style.background = "black";
-//         })) 
-//     })
+function grayColor() { 
+    
+    const boxs = container.querySelectorAll('.box')
+    btnGray.textContent = "GRAY"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
+        }))
+    })
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
+}
+grayColor()
 
-//     buttonContainer.appendChild(btnblack).classList.add('btn')
-// }
+
+function rgbColors() {
+    
+    const boxs = container.querySelectorAll('.box')
+    btnRgb.textContent = "RGB"
+    btnRgb.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+        }))
+    })
+    buttonsContainer.appendChild(btnRgb).classList.add('btn')
+}
+rgbColors()
+
+function blackColor() {
+   
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = 'BLACK';
+    btnBlack.addEventListener('click', function () {
+        boxs.forEach(box => box.addEventListener('mouseover', function() {
+            this.style.background = 'black'
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+blackColor() 
+
+function reSet() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => {
+        box.remove();
+    })
+}
