bd54e225a9e6d85bc2d4ac5f2c0f807bbf3870c1
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-23 09:17:02 +0300

    update

--- Week-03/Day-5/dc/main.js
+++ Week-03/Day-5/dc/main.js
@@ -1,5 +1,42 @@
-let listTasks = [];
+const input = document.querySelector('#add');
+const btn = document.querySelector('#btn');
+const list = document.querySelector('#list');
+var el = document.getElementsByTagName('li');
 
-function addTask(){
-    
+btn.onclick = function{
+    var text = input.value;
+    if(txt == ''){
+        alert('Write something please');
+    }
+    else{
+        li = document.createElement('li');
+        li.innerHTML = txt;
+        list.insertBefore(li,list.childNodes[0]);
+        input.value = '';
+    }
 }
+
+// btn.addEventListener('click', ()=>{
+//     let txt=input.value;
+//     if(txt===""){
+//         alert('Add a task please');
+//     }
+//     else{
+//         let li=document.createElement('li');
+//         li.innerHTML = txt;
+//         list.insertBefore(li, list.childNodes[0]);
+//         input.value='';
+//     }
+// })
+
+list.onclick = function(e){
+    if(e.target.tagName == 'LI'){
+         e.target.classList.toggle('checked');
+    }
+};
+
+// list.addEventListener('click', e=>{
+//     if(e.target.tagname == 'LI'){
+//         e.target.classList.toggle('checked');
+//     }
+// })
