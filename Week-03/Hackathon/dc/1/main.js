a664ad0ae0303065290aec386980777f94d633cb
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-25 00:57:05 +0300

    cleanup

--- Week-03/Hackathon/dc/1/main.js
+++ Week-03/Hackathon/dc/1/main.js
@@ -4,14 +4,14 @@ function Aletter(){
     for (let i = 0; i < 7; i++) {
         for (let i = 0; i < 5; i++) { 
            if (((col==0 || col==4) && row!=0) || ((row==0 || row==3) && (col>0 && col<4))){
-               str1 = str1+"*"
+               str1 = str1+"*";
            }
            else{
-            str1 = str1+" "
+            str1 = str1+" ";
            }  
         }  
     }
 }
+Aletter();
 
-
-console.log()
+console.log(Aletter);
--- Week-03/Hackathon/dc/1/script.py
+++ Week-03/Hackathon/dc/1/script.py
@@ -1,10 +1,11 @@
 str1 = ""
 
-for row in range(7):          ====>rows  
-	for col in range(5):  ====>columns
-		if ((clo==0 or col==4) and row!=0) or ((row==0 or row==3) and (col>0 and col<4))
+for row in range(7):
+	for col in range(5):
+		if ((clo==0 or col==4) and row!=0) or ((row==0 or row==3) and (col>0 and col<4)):
 			str1 = str+"*"
-
 		else: 
 			str1 = str+" "
-	print()
+	
+
+print(str)
