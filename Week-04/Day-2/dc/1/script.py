08f3e23d07a9bfb05ff83693e016e416f40a33cb
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-27 23:32:16 +0300

    python exercises

--- Week-04/Day-2/dc/1/script.py
+++ Week-04/Day-2/dc/1/script.py
@@ -1,25 +1,34 @@
-date_list = []
-birth_date = input("Whats your birthdate (DD/MM/YYYY) ? : ")
-
-
-year = birth_date[6:10]
-
-age = 2021 - int(year)
-last_num = age % 1
-
-print(last_num)
-
- 
-# print(
-# '       ___________       '
-# '      |:H:a:p:p:y:|      '
-# '    __|___________|__    '
-# '   |^^^^^^^^^^^^^^^^^|   '
-# '   |:B:i:r:t:h:d:a:y:|   ' 
-# '   |                 |   '
-# '   ~~~~~~~~~~~~~~~~~~~   '
-# )
-
+birth_date = input('Please input your birthdate: (format: DD/MM/YYYY)')
+d, m, y = list(map(int,birth_date.split('/')))
+date = list(map(int, "27/4/2021".split('/')))
+age = date[2] - y
+birthday_flag = False
+if m >= date[1]:
+    if date[0] > d:
+        age -= 1
+if m == date[1] and d == date[0]:
+    print("happy birthday!!!")
+    birthday_flag = True
+print_string = f'you are {age} years old' 
+if birthday_flag: print_string += ' today'
+print(print_string)
+candles = int(age/10)
+candle_string = 'i' * candles
+while len(candle_string) < 10:
+    candle_string += '_'
+    candle_string = '_' + candle_string 
+if len(candle_string) % 2 == 1:
+    candle_string = candle_string[0:-1]
+cake = f"""
+       _{candle_string}_
+# '    |:H:a:p:p:y:|      '
+# '  __|___________|__    '
+# ' |^^^^^^^^^^^^^^^^^|   '
+# ' |:B:i:r:t:h:d:a:y:|   ' 
+# ' |                 |   '
+# ' ~~~~~~~~~~~~~~~~~~~   '
+"""
+print (cake)
 # The number of candles on the cake should be the last number of the users age, if they are 53, then add 3 candles.
 
 # Bonus : If they were born on a leap year, display two cakes !
--- Week-04/Day-3/dc/1/script.py
+++ Week-04/Day-3/dc/1/script.py
@@ -1,9 +1,28 @@
 text = input("encrypted message : ")
 
-for letter in text:
-    cypher_text += chr(ord(letter) + 3)
+def cipher(text,s):
+    result = ''
 
-    print(cypher_text)
+    for i in range(len(text)):
+        char = text[i]
+
+        if text.str.lower():
+            result += chr((ord(char) + s-65) % 26 + 65)
+
+        else:
+            result += chr((ord(char) + s-97) % 26 + 97)
+
+        return result
+
+s = 3
+
+print ("original text : " + text)
+print ("=============================")
+print ("shifted text : " + cipher(text,s))
+
+
+
+
 #     In cryptography, a Caesar cipher is one of the simplest and most widely known encryption techniques.
 # It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.
 
