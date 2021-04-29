80c351233381f87b3e55a99fa034ee0319218983
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-04-29 15:44:31 +0300

    mini project

---
+++ Week-04/Day-5/ch/1/script.py
@@ -0,0 +1,10 @@
+
+
+
+user_input = input("Add an item in your list : ")
+user_pos = input('write the position number where you want to add your item')
+
+def insert_item():
+   list() 
+
+# Write a script that inserts an item at a defined index in a list.
