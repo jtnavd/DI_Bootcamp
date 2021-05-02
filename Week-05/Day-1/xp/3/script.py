af6be0f10cfd8969190e5bd79d484509619aab4c
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-05-02 15:14:37 +0300

    python exersices

---
+++ Week-05/Day-1/xp/3/script.py
@@ -0,0 +1,25 @@
+class Song():
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song():
+        lyrics = []
+        lyrics.append("There’s a lady who's sure")
+        lyrics.append("all that glitters is gold")
+        lyrics.append("and she’s buying a stairway to heaven")
+
+
+
+# Define a class called Song, it will show the lyrics of a song.
+# Its __init__() method should have two arguments: self and lyrics (a list).
+# Inside your class create a method called sing_me_a_song that prints each element of lyrics on its own line.
+# Create an object, for example:
+
+# stairway= Song(["There’s a lady who's sure",c, "and she’s buying a stairway to heaven"])
+
+
+# Then, call the sing_me_a_song method. The output should be:
+
+# There’s a lady who's sure
+# all that glitters is gold
+# and she’s buying a stairway to heaven
