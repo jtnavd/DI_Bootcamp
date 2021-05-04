1f2b7c0f10e84cd250093195ff4c5b48982cbe6b
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-05-04 18:18:58 +0300

    python

--- Week-05/Day-3/xp_modules/4/script.py
+++ Week-05/Day-3/xp_modules/4/script.py
@@ -5,3 +5,13 @@
 # The function should also display the amount of time left from now until the next upcoming holiday
 #  and print which holiday that is. (Example: the next holiday is in 30 days and 12:03:45 hours).
 # Hint: Start by hardcoding the datetime and name of the upcoming holiday.
+
+
+
+
+
+
+
+
+
+
