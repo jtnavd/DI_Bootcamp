663f383561d74ed9036390b50bd34b5c0c1a0f50
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-05-05 16:27:03 +0300

    python exercises

---
+++ Week-05/Day-4/xp/1/script.py
@@ -0,0 +1,19 @@
+# import json
+# # import list_word
+import random
+
+words = open('list_word.txt', 'r')
+length = input(int('how many words do you want in your sentence?'))
+
+def get_words_from_file():
+    data = words.read()
+    coll = []
+    for i in words:
+        coll.append(i)
+        return coll
+
+def get_random_sentence(lenght):
+    length = input(int('how many words do you want in your sentence?'))
+
+
+# lenght is number of words
