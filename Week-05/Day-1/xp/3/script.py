ea9187f286b2ae06da6d8feff6bc5a56c8a354ab
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-05-09 09:38:36 +0300

    python exercise

--- Week-05/Day-1/xp/3/script.py
+++ Week-05/Day-1/xp/3/script.py
@@ -2,10 +2,13 @@ class Song():
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
-    def sing_me_a_song():
+    def sing_me_a_song(self):
         lyrics = []
         lyrics.append("There’s a lady who's sure")
         lyrics.append("all that glitters is gold")
         lyrics.append("and she’s buying a stairway to heaven")
 
-print(sing_me_a_song())
+    def __str__(self):
+        return '/'.join(self.lyrics)
+
+starway = Song.sing_me_a_song()
---
+++ Week-05/Day-5/mp_2/anagram_checker.py
@@ -0,0 +1,4 @@
+class Anagram():
+    def __init__(self):
+
+        
