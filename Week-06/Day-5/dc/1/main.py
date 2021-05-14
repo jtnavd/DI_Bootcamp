3d89821429e495f5e7058d19a7544cba88fd2edf
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-05-14 14:49:33 +0300

    organize

--- Week-06/Day-5/dc/1/main.py
+++ Week-06/Day-5/dc/1/main.py
@@ -1,21 +1,28 @@
+
+
 class MenuItem:
     def __init__(self,name,price):
         self.name = name
         self.price = price
 
     def save(self):
+        print('save')
         pass
 
     def delete(self):
+        print('delete')
         pass
 
     def update(self):
+        print('update')
         pass
 
     def all(self):
+        print('all')
         pass
 
     def get_by_name(self,name):
+        print('get name')
         pass
 
-    
+print("hello")
